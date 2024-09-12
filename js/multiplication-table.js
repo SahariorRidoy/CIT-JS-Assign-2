0705a48350e06478e6b4446c01881c6528941d7d
MD. Saharior Ridoy <emonhridoy2014@gmail.com>
2024-09-13 01:14:54 +0600

    Solved Assignment 2

---
+++ js/multiplication-table.js
@@ -0,0 +1,8 @@
+
+for(let i=1; i<=10;i++){
+    for (let j = 1; j <=10; j++) {  
+       let multiply=i*j;
+       console.log(i +" X "+j+" = ", multiply); 
+       document.write(i +" X "+j+" = ", multiply ,"<br>"); 
+    }
+}
---
+++ js/negative-while.js
@@ -0,0 +1,6 @@
+let i=-10;
+while (i<=-1) {
+    document.write(i,"<br>");
+    i++;
+    console.log(i);
+}
